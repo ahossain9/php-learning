1d5087e69bf1ef291e9b8ed1a37bbcde271f5cf0
amhossain93 <a00hossain@gmail.com>
2019-10-06 02:30:55 +0600

    Update comparison.php

--- Operators/comparison.php
+++ Operators/comparison.php
@@ -1,1 +1,19 @@
 
+// logical operator
+echo '<br>';
+$food = 'salmon';
+if('salmon' == $food || 'tuna' == $food){
+    echo 'Best Food';
+} else{
+    echo 'Isn\'t good for health';
+}
+
+
+// findout leaf year
+echo '<br>';
+$year = 2000;
+if( $year % 4 == 0 && ( $year %100 !=0 || $year % 400 == 0 )){
+    echo "{$year} is a leaf year";
+} else{
+    echo "{$year} is not a leaf year"; 
+}
